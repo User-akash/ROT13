402cdeea4e597a69320c0bd5e1a85b0708b4f1f6
User-akash <80746921+User-akash@users.noreply.github.com>
2022-10-03 21:21:22 +0600

    Add files via upload
    encryption to decryption algorithm

---
+++ server.js
@@ -0,0 +1,47 @@
+function rot13(){
+    const message = document.querySelector('#myInput').value
+    if(message==""){
+      alert("Empty field to Encrypt.");
+    }
+    var t = message.split('')
+    var key = 'abcdefghijklmnopqrstuvwxyz'
+    var word = ''
+    t.forEach((letter)=>{
+      idx = key.indexOf(letter.toLowerCase());
+      if(idx !== -1){
+        idx < key.length / 2 ?
+        letter == letter.toUpperCase() ?
+        word += key[idx + 13].toUpperCase() :
+        word += key[idx + 13] :
+        letter == letter.toUpperCase() ?
+        word += key[idx - (key.length / 2)].toUpperCase() :
+        word += key[idx - (key.length / 2)];
+      } else {
+        idx === -1 ? word += letter : '';
+      }
+    });
+    document.querySelector('#myMessage').innerText = word
+  }
+  function rot13(){
+    if(document.querySelector('#myInput').value==""){
+      alert("Empty field to decrypt.");
+    }
+    var t = document.querySelector('#myInput').value.split('')
+    var key = 'abcdefghijklmnopqrstuvwxyz'
+    var word = ''
+    t.forEach((letter)=>{
+        idx = key.indexOf(letter.toLowerCase());
+        if(idx !== -1){
+          idx >= key.length / 2 ?
+          letter == letter.toUpperCase() ?
+          word += key[13-(key.length - idx)].toUpperCase() :
+          word += key[13-(key.length - idx)] :
+          letter == letter.toUpperCase() ?
+          word += key[idx + 13].toUpperCase() :
+          word += key[idx + 13];
+        } else {
+          idx === -1 ? word += letter : '';
+        }
+    });
+    document.querySelector('#myMessage').innerText = word
+  }
